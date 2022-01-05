81a72faf5d95696db366c9eb7a943b71bd142713
satoshi-githu <rie1366@yahoo.co.jp>
2022-01-05 11:39:33 +0900

    product-listingの初めのpage

---
+++ src/app/product-listings/product-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
